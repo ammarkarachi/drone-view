6d772447deb66d8310cb9a98d06d31a8cb4464ae
AmmarKarachi <ammkara@amazon.com>
2022-10-06 14:20:16 -0700

    feat: added annotated map

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,22 +1,26 @@
-
-
-import { IFlightLog } from '../Utils/read_csv'
-import { chakra, Heading, HStack, VStack } from '@chakra-ui/react';
-import { MapContainer, Polyline, TileLayer, useMap } from 'react-leaflet';
-import { QueryState } from './QueryBuilder'
-import { useLiveQuery } from 'dexie-react-hooks';
-import { getFlightLogs } from '../Utils/db_wrapper';
-import { IFlightLogRecord } from '../Utils/db';
-import { useEffect, useState } from 'react';
-import React from 'react';
+import { IFlightLog } from "../Utils/read_csv";
+import {
+  chakra,
+  Checkbox,
+  Heading,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
+import { MapContainer, Polyline, TileLayer, useMap } from "react-leaflet";
+import { QueryState } from "./QueryBuilder";
+import { useLiveQuery } from "dexie-react-hooks";
+import { getFlightLogs } from "../Utils/db_wrapper";
+import { IFlightLogRecord } from "../Utils/db";
+import { useEffect, useState } from "react";
+import React from "react";
 const ChakraMapContainer = chakra(MapContainer, {
   baseStyle: {
     h: "full",
     w: "full",
-  }
+  },
 });
 interface MapProps {
-  queryState: QueryState
+  queryState: QueryState;
 }
 function SetBounds(props: { flightLogs: IFlightLog[] }) {
   const map = useMap();
@@ -33,10 +37,16 @@ function SetBounds(props: { flightLogs: IFlightLog[] }) {
   }, new Array<number>());
   if (latitudes.length === 0 || longitudes.length === 0) {
     // just default to the center of the US
-    map.fitBounds([[49.3457868, -124.784407], [24.7433195, -66.9513812]]);
-    return <></>
+    map.fitBounds([
+      [49.3457868, -124.784407],
+      [24.7433195, -66.9513812],
+    ]);
+    return <></>;
   }
-  map.fitBounds([[Math.min(...latitudes), Math.min(...longitudes)], [Math.max(...latitudes), Math.max(...longitudes)]])
+  map.fitBounds([
+    [Math.min(...latitudes), Math.min(...longitudes)],
+    [Math.max(...latitudes), Math.max(...longitudes)],
+  ]);
   return <></>;
 }
 
@@ -44,54 +54,135 @@ function SetSelected(props: { flightLog?: IFlightLog }) {
   const map = useMap();
   const { flightLog } = props;
   if (flightLog === undefined) {
-    return <></>
+    return <></>;
   }
   const latitudes = flightLog.FlightLog.map((log) => log.Latitude);
   const longitudes = flightLog.FlightLog.map((log) => log.Longitude);
   if (latitudes.length === 0 || longitudes.length === 0) {
     // just default to the center of the US
-    map.fitBounds([[49.3457868, -124.784407], [24.7433195, -66.9513812]]);
-    return <></>
+    map.fitBounds([
+      [49.3457868, -124.784407],
+      [24.7433195, -66.9513812],
+    ]);
+    return <></>;
   }
-  map.fitBounds([[Math.min(...latitudes), Math.min(...longitudes)], [Math.max(...latitudes), Math.max(...longitudes)]])
+  map.fitBounds([
+    [Math.min(...latitudes), Math.min(...longitudes)],
+    [Math.max(...latitudes), Math.max(...longitudes)],
+  ]);
   return <></>;
 }
 
 export function Map(props: MapProps) {
   const { queryState } = props;
 
-  const { selectedNames, selectedGenerations, startDate, endDate, duration } = queryState;
-  const flightLogs = useLiveQuery(() => getFlightLogs(selectedNames, selectedGenerations, startDate, endDate, duration), [props.queryState]) || [];
-  const [selectedFlightLog, changeSelectedFlighLog] = useState<IFlightLog | undefined>(undefined)
+  const {
+    selectedNames,
+    selectedGenerations,
+    startDate,
+    endDate,
+    duration,
+  } = queryState;
+  const [isAnnotated, setAnnotation] = useState(false);
+  const flightLogs =
+    useLiveQuery(
+      () =>
+        getFlightLogs(
+          selectedNames,
+          selectedGenerations,
+          startDate,
+          endDate,
+          duration
+        ),
+      [props.queryState]
+    ) || [];
+  const [selectedFlightLog, changeSelectedFlighLog] = useState<
+    IFlightLog | undefined
+  >(undefined);
   useEffect(() => {
     changeSelectedFlighLog(undefined);
-  }, [flightLogs])
+  }, [flightLogs]);
 
-  return <><ChakraMapContainer zIndex={1} key={'map'}  >
-    <TileLayer
-
-      attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-      url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-    />
-
-    {flightLogs.map((r: IFlightLogRecord) => <Polyline opacity={0.65} weight={5} key={r.FlightIdentifier} pathOptions={{ color: r.Color }} positions={r.FlightLog.map(g => [g.Latitude, g.Longitude])} />)}
-    <SetBounds flightLogs={flightLogs} />
-    <SetSelected flightLog={selectedFlightLog} />
-  </ChakraMapContainer>
-    <VStack zIndex={5} position='sticky' bottom={'20'}  width={'50'} align='flex-start' padding={'1'}  >
-      {flightLogs.sort((a, b) => b.Timestamp - a.Timestamp).map((log: IFlightLogRecord, index: number) => <HStack
-      backgroundColor={selectedFlightLog === log ? 'blue.500' : 'none'}
-      padding='0.5'
-        key={log.FlightIdentifier} cursor={'pointer'} onClick={e => {
-          changeSelectedFlighLog(log)
-        }}>
-        <ColorComponent color={log.Color} />
-        <Heading fontSize={'2xs'} >{log.DroneIdentifier}</Heading>
-      </HStack>)}
-    </VStack>
-  </>
+  return (
+    <>
+      <ChakraMapContainer zIndex={1} key={"map"} maxZoom={20}>
+        {isAnnotated ? (
+          <TileLayer
+            key={"annotated"}
+            attribution={
+              "Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
+            }
+            url={
+              "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}"
+            }
+          />
+        ) : (
+          <TileLayer
+            key={"plain"}
+            attribution={
+              "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            }
+            url={
+              "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            }
+          />
+        )}
+        {flightLogs.map((r: IFlightLogRecord) => (
+          <Polyline
+            opacity={0.65}
+            weight={5}
+            key={r.FlightIdentifier}
+            pathOptions={{ color: r.Color }}
+            positions={r.FlightLog.map((g) => [g.Latitude, g.Longitude])}
+          />
+        ))}
+        <SetBounds flightLogs={flightLogs} />
+        <SetSelected flightLog={selectedFlightLog} />
+      </ChakraMapContainer>
+      <VStack
+        zIndex={5}
+        position="sticky"
+        bottom={"20"}
+        width={"50"}
+        align="flex-start"
+        padding={"1"}
+      >
+        {flightLogs
+          .sort((a, b) => b.Timestamp - a.Timestamp)
+          .map((log: IFlightLogRecord, index: number) => (
+            <HStack
+              backgroundColor={selectedFlightLog === log ? "blue.500" : "none"}
+              padding="0.5"
+              key={log.FlightIdentifier}
+              cursor={"pointer"}
+              onClick={(e) => {
+                changeSelectedFlighLog(log);
+              }}
+            >
+              <ColorComponent color={log.Color} />
+              <Heading fontSize={"2xs"}>{log.DroneIdentifier}</Heading>
+            </HStack>
+          ))}
+        <Checkbox
+          isChecked={isAnnotated}
+          size='sm'
+          padding={'1'}
+          backgroundColor={'blackAlpha.500'}
+          onChange={(e) => {
+            setAnnotation(e.target.checked);
+          }}
+        >
+          Show Map Labels
+        </Checkbox>
+      </VStack>
+    </>
+  );
 }
 
 const ColorComponent = ({ color }: { color: string }) => {
-  return <div style={{ width: '10px', height: '10px', backgroundColor: color }}></div>
-}
+  return (
+    <div
+      style={{ width: "10px", height: "10px", backgroundColor: color }}
+    ></div>
+  );
+};
